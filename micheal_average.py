556ad52795b00396b9e514731e2c19bffed5a065
oskar <oskar.harber@outlook.com>
2025-09-30 11:56:17 +0100

    average function made

---
+++ micheal_average.py
@@ -0,0 +1,15 @@
+import csv
+def main():
+    psi=[]
+    with open("test", "r") as f:
+        lines =f.readlines()
+        atom_positions = lines[1:]
+        for atom in atom_positions:
+            data =atom.split()
+            psi.append(float(data[5]))
+            
+    average_psi = np.mean(psi)
+    print(f"the average psi 6 is {average_psi}")
+    
+if __name__ == "__main__":
+    main()
