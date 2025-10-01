34d861ed1fbaa84d020a3537b7fb3822c50065a8
oskar <oskar.harber@outlook.com>
2025-10-01 14:17:42 +0100

    update to converter to find final snapshot

--- lammps_to_michael.py
+++ lammps_to_michael.py
@@ -1,17 +1,25 @@
+
 import csv
 def main():
     coordinates=[]
     with open("dump.LJ", "r") as f:
         lines =f.readlines()
-        atom_positions = lines[9:1009]
-        for atom in atom_positions:
-            data =atom.split()
-            x, y = float(data[2]), float(data[3])
-            coordinates.append([x,y])
+        for i in range(1,102):
+            index1 = i*1009 +9 
+            index2 = index1 + 1000
+            set1 = []
+            atom_positions = lines[index1:index2]
+            for atom in atom_positions:
+                data =atom.split()
+                x, y = float(data[2]), float(data[3])
+                set1.append([x,y])
+            coordinates.append(set1)
+            
 
     with open("pos_file", "w",newline="") as csvf:
         writer =csv.writer(csvf, delimiter=" ")
-        for atom_i in coordinates:
+        final_snapshot = coordinates[-1]
+        for atom_i in final_snapshot:
             writer.writerow([f"{atom_i[0]}",f"{atom_i[1]}"])
 
 
